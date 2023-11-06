2931be4da0dac6aaf4e66809d12f725aec64631c
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2023-11-07 00:06:56 +0530

    Python Class 10 Silicon

---
+++ class_10.py
@@ -0,0 +1,41 @@
+# ***************** Class - 10 *******************
+
+# Strings
+# a = "MCA"
+# b = "MSc"
+
+# string operators -------
+# print(a+b)
+
+# replicate operators --------
+# print(3*a)
+
+# Comparision Operator --------
+# print(a>b) # Compare Char By char (Like M-M, C-S, A-c etc...)
+
+# String Index---------
+# a = "hello silicon"
+# b = a[2:9]
+# print(b)
+# print(a[::2]) # Output = hloslcn
+
+# String Palindrome Check Enter By User...
+# a = input("Enter A Strings : ")
+# b = a[::-1]
+# print(a)
+# print(b)
+
+# if a == b:
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
+
+# Capitalize String
+a= "ilicon"
+print(a.capitalize()) # Make first letter capital...
+
+
+
+
+
+
