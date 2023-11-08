23794631c06c7a047cd97e72da7074ed8dfd7a90
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2023-11-08 20:45:10 +0530

    Python Class - 13 By Nitish Sir...
    Topic Covered:
    1. Loops
    2. Break
    3. Continue
    4. Some Question Solved

---
+++ class_13.py
@@ -0,0 +1,58 @@
+#  break & continue statement in python...
+
+# Break Statements---------
+# Break Statement Is Used To 
+
+# i =1
+# while i<=10:
+#     print('6 * ',(i), '=', 6*i)
+#     if i>=5:
+#         break  # Use Of Break Statement In Python
+#     i=i+1
+
+#Continue Statements--------
+# Continue Statement is used to skip the current iteration of the loop and the control flow of the program goes to the next iteration.
+
+# for i in range(5): # Range Function Runs (n-1) times
+#     if i == 3:
+#         continue  # Skip The Current Iteration...
+#     print(i)
+
+
+# Question:
+#WAP to print the numbers from 1-10 which are odd numbers
+
+# for i in range(10): # Range Function Runs (n-1) times
+#     if i%2 == 0:
+#         continue  # Skip The Current Iteration...
+#     print(i)
+
+
+# Qustion:
+# WAP to find the sum of all +ve numbers less than hundred that are divisible by 7,and break the loop once the sum exceed 1000...
+
+# total = 0
+# for i in range(100):  # also can use range(1,100)
+#     if i%7==0:
+#         total = total + i
+#     if total >= 100:
+#             break
+# print(total)  
+
+
+# for i in range(5):
+#     if i == 3:
+#         pass # Pass Also Called As No Operation Statements
+#     print(i)
+
+# While Loops
+
+# i=1
+# while i<=10:
+#     print(i)
+#     i+=1
+
+# End Of Python Class 13 - With Nitish Sir.
+
+
+
