d90cf0636fb31317cb9e33930deaf19b364633b7
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2024-01-09 21:07:08 +0530

    Loops In Python
    Loops In Python, Practice with for and while loops.

---
+++ loops_in_python.py
@@ -0,0 +1,42 @@
+# ------------Loops In Python -----------
+
+# loops with string ---------
+
+# name = "Sunil"
+# for i in name:
+#     print(i)
+#     if(i == "n"):
+#         print("This Is Special!")
+
+# range function with loops---------
+
+# for k in range(1,9): # it goes n -1 th point numbers
+#     print(k)
+
+# for k in range(20,150):
+#     print(k)
+
+# Range function with parameters---------
+# for k in range(20,0,-5):
+#     print(k)
+
+# -------------- While Loops In Python-----------------
+
+# i = 0
+# while(i < 7):
+#     print(i)
+#     i = i + 1
+# print("Done With Loops!")
+
+# User Input Iterations With Loops......
+
+# i = 0
+# while(i <= 50):
+#     i = int(input("Enter The Number: "))
+#     print(i)
+
+# Decrementing While Loops..........
+# count = 10
+# while(count > 0):
+#     print(count)
+#     count = count - 1
