e2465909be5818c7b0ea4a2f6386220bd9144940
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2024-01-09 19:21:16 +0530

    String Functions In Python
    Practice String Functions In Python Programming Language.

---
+++ python.py
@@ -0,0 +1,58 @@
+# name = "Sunil"
+# for character in name:
+#     print(character)
+
+# print(len(name))
+# print(name[0:5])
+
+# print(name[-3:-1])
+
+# ---- String Methods-----
+# Strings are immutable in python
+
+# print(name.upper())
+# print(name.lower())
+
+# ---------
+
+# a = "!!Apple!!!!!"
+
+# # print(a.rstrip("!"))
+# print(name.replace("Sunil","John"))
+
+# name = "sunil Kumar sahoo"
+# print(name.capitalize())
+# Do first character Upper and others all lower.
+
+# a = "Sunil Kumar!  You are a good boy."
+# print(a.count("a"))
+# print(a.endswith("oy."))
+
+# ------- Find method------
+# print(a.find(" k"))
+
+# name = "elcomeoome"
+# # print(name.isalnum())
+# print(name.isalpha())
+# print(name.islower())
+
+# a = "I am a good boy!\n"
+# print(a.isprintable())
+
+# char = " "
+# print(char.isspace())
+
+# a = "I am a good boy!"
+# print(a.swapcase()) # Capital To Small And Vice Versa
+# print(a.title())
+
+
+
+
+
+
+
+
+
+
+
